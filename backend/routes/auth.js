cd19ca19385b03e3fb0993221420b82603bdb84d
Alexandr Podogas <a.podogas@gmail.com>
2020-12-18 17:34:21 +0300

    delete name requeried from celebrate auth route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,11 +15,11 @@ auth.post('/signin', celebrate({
 }), login);
 auth.post('/signup',  celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    avatar: Joi.string().required().pattern(linkRegExp),
+    avatar: Joi.string().pattern(linkRegExp),
   })
 }), createUser);
 
